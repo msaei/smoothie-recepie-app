c322e46e8aff1afba8cee29e60b6d192eda15817
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-06 10:14:57 -0500

    add recepie form created

--- src/components/AddRecepie.vue
+++ src/components/AddRecepie.vue
@@ -1,17 +1,51 @@
 <template>
-  <h2>Add Recepie</h2>
+  <div class="add-recepie container">
+    <h2 class="center-align indigo-text">Add new recepie</h2>
+    <form @submit.prevent="addRecepie">
+      <div class="field title">
+        <label for="title">Recepie Title:</label>
+        <input type="text" name="title" v-model="title">
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add Ingredient:</label>
+        <input type="text" name="add-ingredient">
+      </div>
+      <div class="field center-align">
+        <button class="btn pink">Add Recepie</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AddRecepie",
   data() {
-    return {};
+    return {
+      title: null
+    };
+  },
+  methods: {
+    addRecepie() {
+      console.log(this.title);
+    }
   }
 };
 </script>
 
 <style>
+.add-recepie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+.add-recepie h2 {
+  margin: 20px auto;
+  font-size: 2em;
+}
+.add-recepie .field {
+  margin: 20px auto;
+}
 </style>
 
 
