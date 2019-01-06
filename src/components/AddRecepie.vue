8aa0c242fb4c3f7711483102c9428c8a4d779bb4
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-05 23:04:42 -0500

    AddRecepie component created and added to route

---
+++ src/components/AddRecepie.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>Add Recepie</h2>
+</template>
+
+<script>
+export default {
+  name: "AddRecepie",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import AddRecepie from '@/components/AddRecepie'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [{
-        path: '/',
-        name: 'Index',
-        component: Index
-    }]
+            path: '/',
+            name: 'Index',
+            component: Index
+        },
+        {
+            path: '/add-recepie',
+            name: 'AddRecepie',
+            component: AddRecepie
+        }
+    ]
 })
