2dcb2673dcb8eedbeaf69f33f5d5bd893decfb8f
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-07 11:10:33 -0500

    form added to the edit recepie

--- src/components/EditRecepie.vue
+++ src/components/EditRecepie.vue
@@ -1,6 +1,26 @@
 <template>
-  <div class="edit-recepie container">
-    <h2 v-if="recepie">Edit {{recepie.title}}</h2>
+  <div v-if="recepie" class="edit-recepie container">
+    <h2>Edit {{recepie.title}}</h2>
+    <form @submit.prevent="editRecepie">
+      <div class="field title">
+        <label for="title">Recepie Title:</label>
+        <input type="text" name="title" v-model="recepie.title">
+      </div>
+      <div class="field" v-for="(ing, index) in recepie.ingredients" :key="index">
+        <label for="ingredient">Ingredient:</label>
+        <input type="text" name="ingredient" v-model="recepie.ingredients[index]">
+        <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add Ingredient:</label>
+        <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
+      </div>
+
+      <div class="field center-align">
+        <p class="red-text" v-if="feedback">{{feedback}}</p>
+        <button class="btn pink">Edit Recepie</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -11,8 +31,29 @@ export default {
   name: "EditRecepie",
   data() {
     return {
-      recepie: null
+      recepie: null,
+      another: null,
+      feedback: null
     };
+  },
+  methods: {
+    editRecepie() {
+      console.log(this.recepie.title, this.recepie.ingredients);
+    },
+    addIng() {
+      if (this.another) {
+        this.recepie.ingredients.push(this.another);
+        this.another = null;
+        this.feedback = null;
+      } else {
+        this.feedback = "You should add an ingredient!";
+      }
+    },
+    deleteIng(ing) {
+      this.recepie.ingredients = this.recepie.ingredients.filter(elm => {
+        return elm != ing;
+      });
+    }
   },
   created() {
     let ref = db
@@ -29,5 +70,26 @@ export default {
 </script>
 
 <style>
+.edit-recepie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+.edit-recepie h2 {
+  margin: 20px auto;
+  font-size: 2em;
+}
+.edit-recepie .field {
+  margin: 20px auto;
+  position: relative;
+}
+.edit-recepie .delete {
+  position: absolute;
+  right: 0px;
+  bottom: 16px;
+  font-size: 1.4em;
+  color: #aaa;
+  cursor: pointer;
+}
 </style>
 
