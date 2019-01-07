3a23f18ff52d4d9b0cdfe39fe74b148825994540
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-07 10:54:08 -0500

    data retrived from database

--- src/components/EditRecepie.vue
+++ src/components/EditRecepie.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="edit-recepie container">
-    <h2>Edit Recepie: {{ this.$route.params.recepie_slug }}</h2>
+    <h2 v-if="recepie">Edit {{recepie.title}}</h2>
   </div>
 </template>
 
 <script>
+import db from "@/firebase/init";
+
 export default {
   name: "EditRecepie",
   data() {
-    return {};
+    return {
+      recepie: null
+    };
+  },
+  created() {
+    let ref = db
+      .collection("smoothies")
+      .where("slug", "==", this.$route.params.recepie_slug);
+    ref.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        this.recepie = doc.data();
+        this.recepie.id = doc.id;
+      });
+    });
   }
 };
 </script>
